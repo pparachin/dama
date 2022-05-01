2ff8b98d0cf617d27207c9a185358e9fce1f61cd
Petr Parachin <petr.parachin@gmail.com>
2022-05-01 21:00:20 +0200

    Figure, stone and lady class

---
+++ stone.py
@@ -0,0 +1,6 @@
+from figure import Figure
+
+
+class Stone(Figure):
+    def __init__(self, position, color, status, label, advantage):
+        super().__init__(position, color, status, label, advantage)
