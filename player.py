13f30afe9bd2a8e973af8262b4ea8b3e180f9f30
Petr Parachin <petr.parachin@gmail.com>
2022-06-09 19:51:32 +0200

    Class for player representation

---
+++ player.py
@@ -0,0 +1,21 @@
+from alias import PlayerColor
+from alias import GameDirection
+
+
+class Player:
+
+    def __init__(self, color, typ, score):
+        self._color = color
+        self._typ = typ
+        self._figures = []
+        self._score = score
+
+    def get_score(self):
+        return self._score
+
+    def set_score(self, score):
+        self._score = score
+
+    def get_color(self):
+        return self._color
+
