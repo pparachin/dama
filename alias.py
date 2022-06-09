ea4e7c8b3ab50d515ea972ecc023ec5d79f88084
Petr Parachin <petr.parachin@gmail.com>
2022-06-09 20:00:21 +0200

    StoneColor class, for stone colors and PlayerType class for Player and PC enum

--- alias.py
+++ alias.py
@@ -9,3 +9,13 @@ class PlayerColor(Enum):
 class GameDirection(Enum):
     WHITE_IS_UP = 0
     WHITE_IS_DOWN = 1
+
+
+class StoneColor(Enum):
+    BLACK = 0
+    WHITE = 1
+
+
+class PlayerType(Enum):
+    PLAYER = 0
+    PC = 1
