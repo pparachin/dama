7a0602359b88273fec69ef4fca6e7aaa06d5156a
Petr Parachin <petr.parachin@gmail.com>
2022-06-09 19:53:26 +0200

    Removed object validator initialization from main (moved to game class) and add game initialization

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from gui import GUI
 from validator import Validator
+from game import Game
 
 
 def main():
-    validator = Validator()
-    GUI.run_game(GUI(800,800,8,100,{},30,(255,255,255),(0,0,0), (255, 0, 52), 250, validator.game_field_to2D(validator.generate_game_field("data/moves.csv"))))
-    
+    Game(0, True)
+
 
 if __name__ == "__main__":
     main()
