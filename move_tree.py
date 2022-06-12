0531a3f3dfa06b7648e75f2052e4fb93fedf6c8c
Prokop Mikulasek <prokop12@volny.cz>
2022-06-12 14:09:21 +0200

    added get_moves function for MoveTrees, very important

--- move_tree.py
+++ move_tree.py
@@ -2,28 +2,28 @@ class MovesTree:
 
     def __init__(self):
         self.root = None
+        self.chosen_move = None
 
-    def set_root_move(self, move):
-        self.root = move
+    def set_root_move(self, input_root):
+        self.root = input_root
 
     def get_root(self):
         return self.root
 
     def add_move(self, move):
-        temp_move = self.root
-        can_continue = True
-        while can_continue:
-            for child in temp_move.children:
-                if child.children:
-                    can_continue = True
-                    temp_move = child
-                    continue
-            can_continue = False
-        temp_move.children.append(move)
-
-    def find_all_possible_moves(self, move):
-        pass
-
+        if self.chosen_move is None:
+            temp_move = self.root
+            can_continue = True
+            while can_continue:
+                for child in temp_move.children:
+                    if child.children:
+                        can_continue = True
+                        temp_move = child
+                        continue
+                can_continue = False
+            temp_move.children.append(move)
+        else:
+            self.chosen_move
 
 class Move:
 
@@ -46,6 +46,13 @@ class Move:
     def set_parent(self, input_parent):
         self.parent = input_parent
 
+    def add_parent(self, parent):
+        if (self.parent is not None and
+           self.parent != parent):
+            return
+        self.parent = parent
+        parent.add_child(self)
+
     def force_birth(self, position):
         offspring = Move([self.data[-1], position], self.figure, self.board_state)
         self.children.append(offspring)
@@ -57,3 +64,46 @@ class Move:
 
     def get_children(self):
         return self.children
+
+
+# ------------------------------------------------
+
+def get_moves(tree: MovesTree):
+    def rec_get_moves(move: Move, list_of_moves, list_copy=[]):
+        '''
+        Recursively called to get all possible moves from root to leaf.
+        Takes on input:
+        move ...            a Move object
+        list_of_moves ...   a list to be filled with all possible root-leaf moves
+        list_copy ...       a copy of current Move's list, it is given to a child after current Move's list updates
+        '''
+        # if it is a first Move, begin list_copy with it, add self data afterwards
+        list_copy.append(move.get_data()[1])
+
+        # if it is a leaf Move, complete current list of squares
+        if len(move.get_children()) == 0:
+            list_of_moves.append(list_copy)
+            return
+
+        # otherwise recursively call itself doing this:
+        # 1) copy list_of_moves, which was given to list_copy argument
+        # 2) send copy of list_of_moves to all children
+        # 3) children recursively update their copies
+        for child in move.children:
+            list_for_child = list_copy.copy()
+            rec_get_moves(child, list_of_moves, list_for_child)
+
+    # short output for empty trees
+    if tree.get_root() is None:
+        return []
+
+    # creating main list of moves
+    list_of_moves = []
+
+    # filling the list
+    rec_get_moves(tree.root, list_of_moves)
+    for move in list_of_moves:
+        move.insert(0, tree.root.data[0])
+
+    # outputing the list
+    return list_of_moves
